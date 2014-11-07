4528eac8d38d951364c25b8935685a6b6fc8c4de
0rel <me@0rel.com>
2014-11-07 06:25:50 +0100

    -lpthread added for linux makefiles.

--- es_linux_lib/test/doit.test_libes.sh
+++ es_linux_lib/test/doit.test_libes.sh
@@ -7,5 +7,5 @@ then
 	exit
 fi
 
-g++ -Wall -I. -L./.. -D__LINUX__ -O3 -o test_libes test_libes.cpp -s -les -lrt -lasound && ./test_libes
+g++ -Wall -I. -L./.. -D__LINUX__ -O3 -o test_libes test_libes.cpp -s -les -lrt -lasound -lpthread && ./test_libes
 read -p "press any key to exit..."
